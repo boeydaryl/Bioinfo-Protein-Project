c7910ef18df5fb34b11f60971612d4590ee5b77b
boeydaryl <boeydaryl@gmail.com>
2018-02-22 11:09:03 +0100

    update for 21 Feb 18, updated Diary and slightly ammended parser script.

--- Project/General/3lineparser.py
+++ Project/General/3lineparser.py
@@ -1,7 +1,7 @@
 #3 Line FASTA Parser
 import pandas as pd
 
-def threelineparser(filename):
+def threelineparser(filename, outputfilename):
     file1 = open(filename, 'r')
     datadict = dict()
     for x in file1:
@@ -13,14 +13,15 @@ def threelineparser(filename):
         elif x.islower() and ">" not in x:
             B = x.replace("\n", "")
             datadict[key] = [A, B]
-    #print(datadict)
     df = pd.DataFrame.from_dict(data=datadict, orient='index')
     #df.rename(columns={'0': 'AA', '1': 'state'})
-    print(df)
-    #df.to_csv('textoutput.csv')
+    #df.to_csv(outputfilename)
+    #df['changed'] = map(lambda x: x.replace("e", 1), df[1])
+    #df[1] = df[1].astype(str).replace('e', 1)
+    print(datadict)
 
 
 
 
 if __name__ == "__main__":
-    threelineparser('/Users/daryl/Documents/Bioinfo-Protein-Project/Project/Datasets/buried-exposed.3line.txt')
+    threelineparser('/Users/daryl/Documents/Bioinfo-Protein-Project/Project/Datasets/testfile.txt', 'fulloutput.csv')
