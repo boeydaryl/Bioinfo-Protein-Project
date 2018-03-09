c0c180e12e92bf59530feea02e1a706842e398ee
boeydaryl <boeydaryl@gmail.com>
2018-03-09 13:36:08 +0100

    updated diary to include statistics from running an MCC and RandomForest, along with new randomforest script.

--- Project/General/randomforest.py
+++ Project/General/randomforest.py
@@ -1,4 +1,5 @@
-#Datasplit
+#Randomforest
+
 import numpy as np
 from sklearn import svm
 from sklearn.svm import SVC
@@ -12,30 +13,28 @@ from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.externals import joblib
+from sklearn.ensemble import RandomForestClassifier
 import SVMInput
 
 def extractor(filename):
     AAList, StateList = SVMInput.threelineparser(filename)
     return AAList, StateList
 
-
-def datasplit(windowsize):
+    
+def RanFor(windowsize):
     e_seq, e_state = SVMInput.windowmaker_encoder(AAList, StateList, windowsize)
     x = np.array(e_seq)
     y = np.array(e_state)
-    x_train, x_test, y_train, y_test = train_test_split(x, y , test_size=0.33, random_state=10)
-    #print(y_train)
-    clf = SVC(C=5, gamma=0.01, kernel='rbf')
-    clf.fit(x_train, y_train)
-    y_predicted = clf.predict(x_test)
-    #print(y_predicted)
-    MatCorr = matthews_corrcoef(y_test, y_predicted)
-    print(MatCorr)
+    seed = 10
+    trees = 100
+    kfold = KFold(n_splits=3, random_state=seed)
+    clf = RandomForestClassifier(n_estimators=trees, max_features=3)
+    RFResults = cross_val_score(clf, x, y, cv=kfold, scoring='average_precision')
+    mean_RFResults = RFResults.mean()
+    print(mean_RFResults)
+
     
-
-
-
-
+    
 if __name__ == '__main__':
     AAList, StateList = extractor('../Datasets/testfilesize50.txt')
-    datasplit(9)
+    RanFor(9)
