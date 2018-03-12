7f012f90e347ebfe784387a4abe4c71eb9620b98
boeydaryl <boeydaryl@gmail.com>
2018-03-12 13:13:42 +0100

    PSSMBash.sh

---
+++ Project/Projects/PSSMBash.sh
@@ -0,0 +1,11 @@
+#PSSM Bash script
+
+cd ../Datasets/FASTA/
+
+for element in *.txt
+
+do
+
+psiblast -query $element -out_ascii_pssm ../PSSM/$element.pssm -db ~/Documents/blastdb/uniprot_sprot.fasta -num_iterations 3 -evalue 0.01
+
+done
