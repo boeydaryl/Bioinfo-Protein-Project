0b61b493aa15d615f517b03ec0913375dedb4670
boeydaryl <boeydaryl@gmail.com>
2018-03-12 11:18:08 +0100

    updated Diary, created FASTAParser.py as a simple script to parse the dataset

---
+++ Project/General/FASTAParser.py
@@ -0,0 +1,22 @@
+#FASTA Parser
+#Works to parse header and sequence from dataset to individual FASTA file
+
+def parser(filename):
+    count = 0
+    with open(filename, 'r') as fh1:
+        all_lines = fh1.readlines()
+    #print(all_lines)
+    header_list = []
+    seq_list = []
+    for i in range(0, len(all_lines)):
+        if '>' in all_lines[i]:
+            count += 1
+            filename1 = '../Datasets/FASTA/' + str(count) + '_FASTA.txt'
+            writefile = open(filename1, 'w')
+            writefile.write(all_lines[i])
+            writefile.write(all_lines[i+1])
+
+            
+            
+if __name__ == "__main__":
+    parser('../Datasets/testfilesize50.txt')
